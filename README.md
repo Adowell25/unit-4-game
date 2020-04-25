be42e6f83a507d0a5826fbb2be6344220fb837b0
Adowell25 <Adowell25@gmail.com>
2020-04-25 08:38:21 -0500

    added crystal clicks to show user number

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # unit-4-game
+
+The crystal game is user versus computer. The computer will randomly generate a number and the user has to equal the sum by selecting the crystals.
+Each game the number will be randomly chosen and the crystals will be given a random value.
+It is up to the user to carefully select each stone to decifer its value to add up the total to the random value selected by the computer.
+Wins will be tracked in the "Wins: " and losses will be tracked in "Losses:"
---
+++ assets/javascript/game.js
@@ -0,0 +1,59 @@
+//The player will be shown a random number   
+
+//Crystals will be assigned a specific number
+
+//When the player clicks on a crystal it will add a specific amout of points to the players total
+
+//Increment user score counter by the crystals random number
+
+//If number goes beyond random computer number then game over
+
+//Player wins if the total equals number of the computer 
+
+//Player loses if score goes beyond random computer number
+
+//When game begins again, there will be a new random number.
+
+//All crystals will have a new value
+
+//User score resets to zero
+
+//Wins and losses should remain on the screen until the page is refreshed.
+
+var wins = 0;
+var losses = 0;
+// var computerNumber = [];
+
+//for (var i =0; i<3; i++){
+var randomNumber = Math.floor(Math.random() * 12)+ 1;
+var computerNumber = Math.floor(Math.random() * 103)+19;
+computerNumber = randomNumber + computerNumber;
+
+//}
+
+
+$(document).ready(function(){
+    $("#computerRandoNum").text(computerNumber)
+});
+
+$("#blue").on("click", function(){
+    $("#userNum").append("Your total number is: " + randomNumber);
+
+});
+
+$("#green").on("click", function(){
+    $("#userNum").append("Your total number is: " + randomNumber);
+});
+
+$("#orange").on("click", function(){
+    alert("test");
+});
+
+$("#red").on("click", function(){
+    alert("test");
+});
+
+
+
+
+
